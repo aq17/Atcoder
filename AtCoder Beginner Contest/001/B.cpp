297c2153ccd23067faa65e3ad2b6019979aaa5f5
aq17 <aqneco@gmail.com>
2019-01-01 21:37:54 +0900

    happy new year

--- AtCoder Beginner Contest/001/B.cpp
+++ AtCoder Beginner Contest/001/B.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
 int main(){
@@ -8,6 +9,11 @@ int main(){
 
   if(m < 100) a = "00";
   else if(m >= 100 && m <= 5000){
+    if(m / 100 < 10) a = "0" + to_string(m/100);
+    else a = to_string(m/100);
+  }else if(m >= 6000 && m <= 30000) a = to_string(m/1000+50);
+  else if(m>= 35000 && m <= 70000) a = to_string((m/1000-30)/5+80);
+  else if(m>70000) a = "89";
 
-  }else if(m >= 6000 && m <= 30000) a = string()
+  cout << a << endl;
 }
