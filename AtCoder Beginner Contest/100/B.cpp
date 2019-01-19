93c03b1d9a875b7afd7cf5620725a3a555f9e6c8
aq17 <aqneco@gmail.com>
2019-01-19 21:31:18 +0900

    わからん

---
+++ AtCoder Beginner Contest/100/B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int d,n;
+  cin >> d >> n;
+  int basic = pow(100,d);
+  if (n==100){
+    n = 99;
+  int ans = basic*n;
+  cout << << endl;
+  return 0;
+}
+
+//わからん！！
